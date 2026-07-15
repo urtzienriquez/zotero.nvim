885464f22a0b992c3242e6307a2774400c874962
Urtzi Enriquez-Urzelai <urtzi.enriquez@gmail.com>
2026-07-15 19:10:15 +0200

    add :ZoteroMaxItems to change max n of items shown on the fly

--- plugin/zotero.lua
+++ plugin/zotero.lua
@@ -14,3 +14,14 @@ end, { desc = "Zotero debug info" })
 vim.api.nvim_create_user_command("ZoteroImport", function(opts)
   require("zotero.api").import_pdf(opts.args)
 end, { desc = "Import a PDF into Zotero", nargs = 1, complete = "file" })
+
+vim.api.nvim_create_user_command("ZoteroMaxItems", function(opts)
+  local n = tonumber(opts.args)
+  if not n or n < 1 then
+    vim.notify("zotero: max_items must be a positive integer", vim.log.levels.ERROR)
+    return
+  end
+  require("zotero.config").options.max_items = n
+  vim.notify("zotero: max_items set to " .. n, vim.log.levels.INFO)
+  require("zotero.ui.items").fetch_and_render(true)
+end, { nargs = 1, desc = "Set the maximum number of items to display (e.g. :ZoteroMaxItems 100)" })
